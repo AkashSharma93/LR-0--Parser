0dbea3ea0e6c1375fa942196deeda2b9186c8baf
Suigetsu <present.akash@gmail.com>
2014-04-03 21:03:52 +0530

    Added code to display more info rather than just the reduction steps. (Like Stack Contents and stuff)

--- parse.h
+++ parse.h
@@ -49,27 +49,78 @@ int get_length(char *production) {
 	return j;
 }
 
+void get_stack_contents(char *t) {
+	int i;
+	char c[5];
+
+	strcpy(t, "$");
+
+	for(i = 0; i <= stack.top; i++) {
+		int n = stack.states[i];
+		sprintf(c, "%d", n);
+		strcat(t, c);
+	}
+}
+
+void get_remaining_input(char *string, int index, char *t) {
+	int i, j;
+
+	for(i = index, j = 0; string[i] != '\0'; i++, j++)
+		t[j] = string[i];
+
+	t[j] = '\0';
+}
+
+void print_contents(char *string, int index, char *matched_string) {
+	char t1[20], t2[20];
+
+	get_stack_contents(t1);
+	get_remaining_input(string, index, t2);
+
+	printf("\t|  %-25s  |  %-25s  |   %25s  |  \t", t1, matched_string, t2);
+}
+
 void parse() {
 	char string[100];
+	char matched_string[100];
+
 	printf("\nEnter a string: ");
 	scanf("%s", string);
 
 	strcat(string, "$");
+	matched_string[0] = '\0';
 	
 	printf("\nThe reduction steps for the given string are as follows...\n\n");
+
+	printf("\t|  %-25s  |  %-25s  |  %25s  |  \t%-30s\n\n", "Stack", "Matched String", "Input String", "Action");
 	
-	int index = 0;
+	int index = 0, m_index = 0;
 	
 	while(1) {
 		char a = string[index];
 
-		if(table.ACTION[get_top()][get_pos(0, a)][0] == 'S') {
+		print_contents(string, index, matched_string);
+
+		if(table.ACTION[get_top()][get_pos(0, a)][0] == 'S') {		//Shift Action.
 			int t = get_int(table.ACTION[get_top()][get_pos(0, a)]);
 			push(t);
 			index++;
+
+			//Printing the result.
+			char t1[20];
+			char state[5];
+
+			strcpy(t1, "Shift ");
+			sprintf(state, "%d", t);
+			strcat(t1, state);
+
+			matched_string[m_index++] = a;
+			matched_string[m_index] = '\0';
+
+			printf("%-30s\n", t1);
 		}
 
-		else if(table.ACTION[get_top()][get_pos(0, a)][0] == 'R') {
+		else if(table.ACTION[get_top()][get_pos(0, a)][0] == 'R') {	//Reduce Action.
 			int i, j = get_int(table.ACTION[get_top()][get_pos(0, a)]);
 
 			for(i = 0; i < get_length(augmented_grammar[j]); i++)
@@ -80,13 +131,25 @@ void parse() {
 
 			push(table.GOTO[t][get_pos(1, A)]);
 
-			printf("%s\n", augmented_grammar[j]);
+			//Printing the result.
+			m_index -= get_length(augmented_grammar[j]);
+			matched_string[m_index++] = A;
+			matched_string[m_index] = '\0';
+
+			char t1[20];
+			strcpy(t1, "Reduce by ");
+			strcat(t1, augmented_grammar[j]);
+
+			printf("%-30s\n", t1);
 		}
 
-		else if(table.ACTION[get_top()][get_pos(0, a)][0] == 'a')
+		else if(table.ACTION[get_top()][get_pos(0, a)][0] == 'a') {	//Acceptance.
+			printf("%-30s\n", "Accept!!");
 			break;
+		}
 
-		else {
+		else {								//Error.
+			printf("%-30s\n", "Error!!\n\n");
 			printf("String doesn't belong to the language of the particular grammar!\n");
 			exit(0);
 		}
